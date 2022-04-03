ccdc9f3faf3f9225436180e53eeb0750769de6f0
SamsAgency <samstore637@gmail.com>
2022-04-03 21:19:57 +0300

    created a form textfield UI

---
+++ src/components/formsUI/textfield/textfield.js
@@ -0,0 +1,26 @@
+import { TextField } from "@mui/material";
+import React from 'react';
+import { useField } from "formik";
+
+const TextfieldWrapper = ({name, ...otherProps}) => {
+
+	const [field, meta] = useField(name)
+
+	const configTextField = {
+		...field,
+		...otherProps,
+		fullWidth: true,
+		variant: "outlined"
+	}
+
+	if (meta && meta.touched && meta.error){
+		configTextField.error = true
+		configTextField.helperText = meta.error
+	}
+
+	return (
+		<TextField {...configTextField}/>
+	)
+}
+
+export default TextfieldWrapper
--- src/pages/dashboard/storage/addstoragemodal/addstorageform.js
+++ src/pages/dashboard/storage/addstoragemodal/addstorageform.js
@@ -1,41 +1,98 @@
-import { Button, Grid, TextField } from "@mui/material"
+import { Box, Button, Grid, Typography } from "@mui/material"
 import { styled } from "@mui/system";
+import { Formik, Form } from "formik";
 import React from 'react';
-import { addStorageFormContent } from "./addStorageformcontent"
+import * as Yup from "yup";
+import TextField from "../../../../components/formsUI/textfield/textfield"
+// import { addStorageFormContent } from "./addStorageformcontent"
 
-const StyledTextField = styled(TextField)(({theme}) => ({
-	// width: "100%",
-	marginTop: "10px",
-	[`& fieldset`]:{
-		borderRadius: theme.shape.default
-	  }
+
+const StyledWrapper = styled(Box)(({theme}) => ({
+	padding: 10
 }))
+
 
 const StyledAddButton = styled(Button)(({theme}) => ({
-	borderRadius: theme.shape.default,
-	margin: "20px",
+	borderRadius: 3,
 	textAlign: "left",
-	minWidth: "10vw",
-	minHeight: "50px"
+	padding: "8px 25px"
 }))
+
+const INITIAL_FORM_STATE = {
+	firstname: "",
+	lastname: "",
+	email: "",
+	telephone: "",
+	company: ""
+}
+
+const FORM_VALIDATION = Yup.object().shape({
+	firstname: Yup.string().required(),
+	lastname: Yup.string().required(),
+	email: Yup.string().email().required(),
+	telephone: Yup.string().required(),
+	company: Yup.string().required(),
+})
 
 const AddStorageForm = () => {
 	return (
-		<Grid container spacing={2}>
-			{addStorageFormContent.map((input, i) => (
-				<Grid item lg={4} md={6} sm={12}>
-					<StyledTextField
-						label={input.label}
-						variant="outlined"
-						fullWidth
-						required
-					/>
-				</Grid>
-			))}
+		<StyledWrapper container spacing={2}>
+
+			<Formik
+				initialValues={{
+					...INITIAL_FORM_STATE
+				}}
+				validationSchema={ FORM_VALIDATION }
+				onSubmit = {values => {
+					console.log(values)
+				}}
+			>
+				<Form>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<Typography variant="h5" color="blue" gutterBottom>
+								Client Information
+							</Typography>
+						</Grid>
+						
+						{
+							["firstname", "lastname"].map((el, i) => (			
+								<Grid key={i} item sm={6} xs={12}>
+									<TextField name={el} label={el}/>
+								</Grid>
+							))
+						}
+						<Grid item xs={12}>
+							<TextField name="email" label="Email"/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField name="telephone" label="Telephone"/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField name="company" label="Company"/>
+						</Grid>
+
+						<Grid item xs={12}>
+							<Typography variant="h5" color="blue" gutterBottom>
+								Product Information
+							</Typography>
+						</Grid>
+
+						<Grid item xs={12}>
+							<Typography variant="h5" color="blue" gutterBottom>
+								Storage Information
+							</Typography>
+						</Grid>
+
+					</Grid>
+				</Form>
+
+			</Formik>
+			
 			<StyledAddButton variant="contained">
 				Submit
 			</StyledAddButton>
-		</Grid>
+		</StyledWrapper>
 	)
 }
 
