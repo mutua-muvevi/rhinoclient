ca9905d08f62799b94494d46662b3aa88bfbece2
SamsAgency <samstore637@gmail.com>
2022-05-25 14:50:14 +0300

    fetching direct from the server

--- src/redux/quotation/quotationactions.js
+++ src/redux/quotation/quotationactions.js
@@ -1,33 +1,33 @@
 import axios from "axios";
-import storageQuotationTypes from "./quotationtypes";
+import quotationTypes from "./quotationtypes";
 
 export const gettingQuotation = () => ({
-	type: storageQuotationTypes.GET_ALL_QUOTATION_START,
+	type: quotationTypes.GET_ALL_QUOTATION_START,
 })
 
 export const getAllQuotationSuccess = (storage) => ({
-	type: storageQuotationTypes.GET_ALL_QUOTATION_SUCCESS,
+	type: quotationTypes.GET_ALL_QUOTATION_SUCCESS,
 	payload: storage,
 })
 
 export const getAllQuotationFail = (errMessage) => ({
-	type: storageQuotationTypes.GET_ALL_QUOTATION_FAIL,
+	type: quotationTypes.GET_ALL_QUOTATION_FAIL,
 	payload: errMessage,
 })
 
 
 
 export const postingQuotation = () => ({
-	type: storageQuotationTypes.POST_QUOTATION_START,
+	type: quotationTypes.POST_QUOTATION_START,
 })
 
 export const postQuotationSuccess = (values) => ({
-	type: storageQuotationTypes.POST_QUOTATION_SUCCESS,
+	type: quotationTypes.POST_QUOTATION_SUCCESS,
 	payload: values,
 })
 
 export const postQuotationFail = (errMessage) => ({
-	type: storageQuotationTypes.POST_QUOTATION_FAIL,
+	type: quotationTypes.POST_QUOTATION_FAIL,
 	payload: errMessage,
 })
 
