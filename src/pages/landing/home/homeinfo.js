5c84c30683be81827cf527036b9a4f55b9a1aed1
SamsAgency <samstore637@gmail.com>
2022-06-28 14:35:06 +0300

    new video with new styles

---
+++ src/pages/landing/home/homeinfo.js
@@ -0,0 +1,40 @@
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+export const homeInfo = {
+	title: "Rhino John Prime Metals",
+	paragraph: [
+		`
+			Rhinojohn Prime Metal International Limited is a global company that has grown 
+			to become one of the leading players in precious metals and stones value chain.
+			We focus on trading, safekeeping, logistics and delivery of these valuable products.
+		`,
+		`
+			Our team of globally exposed experts have combined experiece of more than 20 years 
+			in this trade and guarantee continous and flawless transactions. They will link you 
+			to the right actors and dealers in the trade; from Miners (Artisanal and Industrial), 
+			Buyers, Sellers, Intermediaries, Refiners and Gemologist
+		`
+	]
+}
+
+export const socialIcons = [
+	{
+		icon : <FacebookIcon/>,
+		link: "https://www.facebook.com"
+	},
+	{
+		icon : <TwitterIcon/>,
+		link: "https://www.twitter.com"
+	},
+	{
+		icon : <InstagramIcon/>,
+		link: "https://www.instagram.com"
+	},
+	{
+		icon : <LinkedInIcon/>,
+		link: "https://www.linkedin.com/"
+	},
+]
